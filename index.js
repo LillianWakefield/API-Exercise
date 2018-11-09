524ced6e1dd6f054a400581c9f9ccc3333b4002b
Lillian Wakefield <37185466+LillianWakefield@users.noreply.github.com>
2018-11-08 22:03:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-//const myList = document.getElementById('myList');
 var listItem = '';
 function getNames() {
     $.get('https://api.github.com/users/lillianwakefield/repos', function(data){       
     data.forEach(repo =>{
                 listItem +=  '<li>' + repo.name + '</li>';
-                $('#myList').html(listItem);;
+                $('#myList').html(listItem);
             })
         });
            
